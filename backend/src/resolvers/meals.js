43f4641352ee8f23d8da44762393ca7bf8030eb6
Metashaker <emilianopb92@gmail.com>
2021-11-21 13:58:38 -0600

    feat() meals query returning all data coorectly. Pending indexed queries

--- backend/src/resolvers/meals.js
+++ backend/src/resolvers/meals.js
@@ -5,14 +5,15 @@ import { CSVToJSON } from '../utils/data-parsing'
 const resolvers = {
   Query: {
     meals: async (parent, args, context, info) => {
-      const meals = await fs.promises.readFile(path.join(`${__dirname}/../db/`, "meals.csv"),
+      let meals = await fs.promises.readFile(path.join(`${__dirname}/../db/`, "meals.csv"),
         'utf-8', async (err, data) => {
           if (err) {
             throw err
           }
           return data
         })
-        console.log(CSVToJSON(meals))
+        meals = CSVToJSON(meals).filter(meal => meal?.title)
+        console.log(meals)
       return meals
     }
   },
