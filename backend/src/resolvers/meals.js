9d0a4d40df3a9d002edf9d321e8bfa56719b462f
Metashaker <emilianopb92@gmail.com>
2021-11-21 13:49:09 -0600

    feat() csv file correctly parsed asynchronously

--- backend/src/resolvers/meals.js
+++ backend/src/resolvers/meals.js
@@ -1,11 +1,22 @@
+import fs from "fs"
+import path from "path"
+import { CSVToJSON } from '../utils/data-parsing'
 
 const resolvers = {
   Query: {
     meals: async (parent, args, context, info) => {
-
+      const meals = await fs.promises.readFile(path.join(`${__dirname}/../db/`, "meals.csv"),
+        'utf-8', async (err, data) => {
+          if (err) {
+            throw err
+          }
+          return data
+        })
+        console.log(CSVToJSON(meals))
+      return meals
     }
   },
   //Mutation: {}
 }
-
 export default resolvers
+
