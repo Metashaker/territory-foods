7d5ab12cebbe9481a31fa1b63a340b6ce0d6c9c3
Metashaker <emilianopb92@gmail.com>
2021-11-21 23:34:08 -0600

    feat() added logic to close a dropdown when user clicks one and the first one is open

--- frontend/src/components/filters/filters/Filters.js
+++ frontend/src/components/filters/filters/Filters.js
@@ -3,17 +3,27 @@ import { FiltersContainer, FilterButton, FiltersDropdown } from "./styles";
 import DownIcon from "../../../assets/tf-down.svg";
 
 const Filters = () => {
-  const [isTagsOpen, setTagsOpen] = useState(false);
+  const [areTagsOpen, setTagsOpen] = useState(false);
   const [areMealTypesOpen, setMealTypesOpen] = useState(false);
 
-  const toggleTagsDropdown = () => setTagsOpen(!isTagsOpen);
-  const toggleMealTypesDropdown = () => setMealTypesOpen(!areMealTypesOpen);
+  const toggleTagsDropdown = () => {
+    if (areMealTypesOpen) {
+      setMealTypesOpen(false);
+    }
+    setTagsOpen(!areTagsOpen);
+  };
+  const toggleMealTypesDropdown = () => {
+    if (areTagsOpen) {
+      setTagsOpen(false);
+    }
+    setMealTypesOpen(!areMealTypesOpen);
+  };
   return (
     <FiltersContainer>
       <FilterButton onClick={toggleTagsDropdown} isFirst>
         <p>Dietary preferences</p>
         <img src={DownIcon} alt="Down icon" />
-        <FiltersDropdown isOpen={isTagsOpen} />
+        <FiltersDropdown isOpen={areTagsOpen} />
       </FilterButton>
 
       <FilterButton onClick={toggleMealTypesDropdown}>
